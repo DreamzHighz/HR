f7ebf68c3e3e36f5188361c2eba1ece5b9f4f5dd
Porames Wongsarat <porames.won@mch.mfu.ac.th>
2019-08-13 15:03:23 +0700

    Enter

--- assets/js/page1.js
+++ assets/js/page1.js
@@ -1,13 +1,21 @@
+$(document).ready(function () {
+    var id = sessionStorage.getItem("userId");
 
-var id = sessionStorage.getItem("userId");
+    $.ajax({
+        method: "POST",
+        url: "../assets/php/page1.php",
+        data: { userId: id },
+        success: function (result) {
+            var result = JSON.parse(result);
 
-$.ajax({
-    method: "POST",
-    url: "../assets/php/page1.php",
-    data: { userId: id },
-    success: function (result) {
-        var result = JSON.parse(result);
-        alert(result[0]['name_user']);
-    }
+            $("#fullName").val(result[0]['name_user']);
+            $("#position").val(result[0]['position_user']);
+            $("#department").val(result[0]['department_user']);
+            $("#group").val(result[0]['group_user']);
+            $("#startDate").val(result[0]['startdate_user']);
+
+        }
+    });
 });
+
 
